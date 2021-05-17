b3b8aaa6275f54b560ce66886b8a24def2371a57
Stefan Petersson <stefan.petersson@demoskop.se>
2021-05-17 08:55:37 +0200

    Incremental update

---
+++ R/sweden.R
@@ -0,0 +1,18 @@
+# - ----------------------------------------------------------------------- - #
+# -
+# -
+# -
+# - ----------------------------------------------------------------------- - #
+#' @param level What administrative level to be used in the map c("county",
+#'              "municipality")
+#' @param map  Search path to .shp file (and .dbf, .prj and .shx)
+#' @param data Data vector ordered by administrative region
+#' @param type Should the color be ranked or relative c("rank", "relative")
+#' @param color Base color to use
+#
+sweden <- function(map, data, type = "relative", level = "county", color = "#000000") {
+
+
+}
+# - ----------------------------------------------------------------------- - #
+# - EOF @ 2021-05-16
